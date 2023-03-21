4b1181f47bee5c19b53e0c8df7faf4f20404c4de
ayerteyegodfred <ayerteyegodfred@gmail.com>
2023-03-21 09:26:16 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * main - Prints Holberton as a message.
+ * print_alphabet - print alphabet
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
-int main(void)
-{
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
 	{
-		_putchar(str[count]);
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
-	return (0);
 }
